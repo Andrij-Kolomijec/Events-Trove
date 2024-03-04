6fec99d7be84fac3d4f6026ee7332e51dc2f632d
Difficins <a.kolomijec@seznam.cz>
2024-03-04 21:41:50 +0100

    Added error message

--- client/src/components/Newsletter.tsx
+++ client/src/components/Newsletter.tsx
@@ -1,19 +1,25 @@
 import { useFetcher } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 export default function Newsletter() {
   const fetcher = useFetcher();
   const { data, state } = fetcher;
+  const emailRef = useRef<HTMLInputElement | null>(null);
 
   useEffect(() => {
     if (state === "idle" && data && data.message) {
       window.alert(data.message);
+      emailRef.current!.value = "";
+    } else if (state === "idle" && data && data.error) {
+      window.alert(data.error);
     }
   }, [data, state]);
 
   return (
     <fetcher.Form method="post" action="/newsletter">
       <input
+        ref={emailRef}
+        id="newsletter"
         type="email"
         name="email"
         placeholder="Sign up for the newsletter"
