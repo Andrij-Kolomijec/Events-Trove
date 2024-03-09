2d87836608c75de47663dc44cc91a39dea528862
Difficins <a.kolomijec@seznam.cz>
2024-03-09 21:22:28 +0100

    Added events route error page

---
+++ client/src/routes/EventsError.tsx
@@ -0,0 +1,10 @@
+export default function EventsError() {
+  return (
+    <div id="main">
+      <h1 style={{ marginTop: "2rem" }}>Something went wrong</h1>
+      <h3 style={{ textAlign: "center", padding: "1rem" }}>
+        Unable to load any events
+      </h3>
+    </div>
+  );
+}
